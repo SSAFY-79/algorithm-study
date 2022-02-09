8b9bd6bc702bb6d7e3789b471ff38e721f3ceedc
minje0204 <minje0204>
2022-02-10 00:46:45 +0900

    1913. 달팽이 sovled

--- src/algorithm/minje/week2/boj/q1913/Main.java
+++ src/algorithm/minje/week2/boj/q1913/Main.java
@@ -1,44 +1,49 @@
+/*
+메모리 : 115160KB
+시간 : 396ms
+ */
+
 package algorithm.minje.week2.boj.q1913;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
 
     static int[] dr = {1, 0, -1, 0};
     static int[] dc = {0, 1, 0, -1};
-    static int N, num;
-    static int[][] snale;
     static int[] numIdx;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-        N = Integer.parseInt(br.readLine());
-        num = Integer.parseInt(br.readLine());
-        snale = new int[N][N];
+        int N = Integer.parseInt(br.readLine());
+        int num = Integer.parseInt(br.readLine());
+        int[][] snale = new int[N][N];
+        int r = 0, c = 0, d = 0, count = N * N;
 
-        recursive((int) Math.pow(N, 2), 0, 0, 0);
+        while (count != 0) {
+            if (count == num) {
+                numIdx = new int[]{r+1,c+1};
+            }
+            snale[r][c] = count--;
+            if (r + dr[d] < 0 || r + dr[d] >= N || c + dc[d] < 0 || c + dc[d] >= N || snale[r + dr[d]][c + dc[d]] != 0) {
+                d = (d + 1) % 4;
+            }
+            r += dr[d];
+            c += dc[d];
+        }
 
         for (int[] a : snale) {
             for (int b : a) {
-                System.out.print(b + " ");
+                sb.append(b + " ");
             }
-            System.out.println();
+            sb.append("\n");
         }
-        System.out.println(numIdx[0]+" "+numIdx[1]);
-    }
+        sb.append(numIdx[0] + " " + numIdx[1]);
 
-    static void recursive(int count, int d, int r, int c) {
-        if(count == num) numIdx = new int[]{r+1,c+1};
-        if (count == 0) return;
-        snale[r][c] = count--;
-        if (r + dr[d] >= 0 && r + dr[d] < N && c + dc[d] >= 0 && c + dc[d] < N && snale[r + dr[d]][c + dc[d]] == 0) {
-            recursive(count, d, r + dr[d], c + dc[d]);
-
-        } else {
-            d = (d + 1) % 4;
-            recursive(count, d, r + dr[d], c + dc[d]);
-        }
+        bw.write(sb.toString());
+        bw.flush();
     }
 }

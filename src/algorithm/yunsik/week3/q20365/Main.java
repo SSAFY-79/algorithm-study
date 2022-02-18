6deed1791a0a441070e48869592fd1745a24c0a7
kangyunsik <re12io@ajou.ac.kr>
2022-02-18 22:56:30 +0900

    code: 강윤식 BOJ 20365 그리디 `블로그2`

---
+++ src/algorithm/yunsik/week3/q20365/Main.java
@@ -0,0 +1,24 @@
+package algorithm.yunsik.week3.q20365;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        char[] input = br.readLine().toCharArray();
+        int cnt = 1;
+        char startColor = input[0];
+        char currentColor = input[0];
+
+        for (int idx = 0; idx < n; idx++) {
+            while (idx < n && input[idx] == currentColor) idx++;
+            if (idx < n && currentColor == startColor) cnt++;
+            currentColor = (char)('R' + 'B' - currentColor);
+        }
+
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+    }
+}

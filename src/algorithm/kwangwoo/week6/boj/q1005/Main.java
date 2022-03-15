73955c067a7c60d8643712ee0f6a277b9b0d4280
BumBamBi <dlrhkddn0325@gmail.com>
2022-03-15 18:09:44 +0900

    code : 이광우 Programmers 2020카카오 인턴십 경주로 건설

---
+++ src/algorithm/kwangwoo/week6/boj/q1005/Main.java
@@ -0,0 +1,19 @@
+package algorithm.kwangwoo.week6.boj.q1005;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int TCs = sc.nextInt();
+        int N;
+        int M;
+
+        for (int curTC = 0; curTC < TCs; curTC++) {
+            
+        }
+
+        sc.close();
+    }
+}
